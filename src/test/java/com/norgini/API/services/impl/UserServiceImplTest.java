de453f965e419712bd416bbf77e80a4ffb0e939f
jnorgini <jnorgini@gmail.com>
2023-02-05 15:30:20 -0300

    100% coverage da classe DataIntegrityViolationException

--- src/test/java/com/norgini/API/services/impl/UserServiceImplTest.java
+++ src/test/java/com/norgini/API/services/impl/UserServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -21,11 +22,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.norgini.API.domain.User;
 import com.norgini.API.dto.UserDTO;
 import com.norgini.API.repositories.UserRepository;
+import com.norgini.API.services.exceptions.DataIntegrityViolationException;
 import com.norgini.API.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 class UserServiceImplTest {
-	
+
 	private static final Integer ID = 1;
 	private static final String NAME = "Juliana";
 	private static final String EMAIL = "juliana@gmail.com";
@@ -35,13 +37,13 @@ class UserServiceImplTest {
 
 	@InjectMocks
 	private UserServiceImpl service;
-	
+
 	@Mock
 	private UserRepository repository;
-	
+
 	@Mock
 	private ModelMapper mapper;
-	
+
 	private User user;
 	private UserDTO userDTO;
 	private Optional<User> optionalUser;
@@ -97,6 +99,20 @@ class UserServiceImplTest {
 	}
 
 	@Test
+	void whenCreateThenReturnAnDataIntegrityViolationException() {
+		when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+	
+		try {
+			optionalUser.get().setId(2);
+			service.create(userDTO);
+		} catch (Exception ex) {
+			assertEquals(DataIntegrityViolationException.class, ex.getClass());
+			assertEquals("E-mail já cadastrado no sistema", ex.getMessage());
+		}
+		
+	}
+
+	@Test
 	void testUpdate() {
 		fail("Not yet implemented");
 	}
@@ -105,13 +121,13 @@ class UserServiceImplTest {
 	void testDelete() {
 		fail("Not yet implemented");
 	}
-	
+
 	private void startUser() {
 		user = new User(ID, NAME, EMAIL, PASSWORD);
 		userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
 		optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
 	}
-	
+
 	@Test
 	void whenFindByIdThenReturnAnObjectNotFoundException() {
 		when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
