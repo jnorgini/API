a902e809a8e8ae71988265f067fcc9871f8dc718
jnorgini <jnorgini@gmail.com>
2023-02-05 20:29:43 -0300

    100% coverage das classes ResourceExceptionHandler e StandardError

--- src/test/java/com/norgini/API/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/com/norgini/API/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,8 +1,10 @@
 package com.norgini.API.resources.exceptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,12 +15,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import com.norgini.API.services.exceptions.DataIntegrityViolationException;
 import com.norgini.API.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 	
 	private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+	private static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
 	
 	@InjectMocks
 	private ResourceExceptionHandler exceptionHandler;
@@ -40,11 +44,24 @@ class ResourceExceptionHandlerTest {
 		assertEquals(StandardError.class, response.getBody().getClass());
 		assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
 		assertEquals(404, response.getBody().getStatus());
+		assertNotEquals("/user/2", response.getBody().getPath());
+		assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
 	}
 
 	@Test
-	void testDataIntegrityViolationException() {
-		fail("Not yet implemented");
+	void whenDataIntegrityViolationExceptionThenReturnAnResponseEntity() {
+		ResponseEntity<StandardError> response = exceptionHandler
+				.dataIntegrityViolationException(
+						new DataIntegrityViolationException(E_MAIL_JA_CADASTRADO_NO_SISTEMA), 
+						new MockHttpServletRequest());
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(StandardError.class, response.getBody().getClass());
+		assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, response.getBody().getError());
+		assertEquals(400, response.getBody().getStatus());
 	}
 
 }
