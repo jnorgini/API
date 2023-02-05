95e8ed092b02577348fc0fadd5ac7139856fca52
jnorgini <jnorgini@gmail.com>
2023-02-04 23:51:04 -0300

    UserDTO

--- src/main/java/com/norgini/API/dto/UserDTO.java
+++ src/main/java/com/norgini/API/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.norgini.API.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,8 @@ public class UserDTO {
 	private Integer id;
 	private String name;
 	private String email;
+	
+	@JsonIgnore
 	private String password;
 
 }
